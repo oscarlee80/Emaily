3ca29304e5830e07c9a3f3254a46863436624dcb
Oscar Lee <oscarlee80@gmail.com>
2020-12-11 11:21:04 -0300

    mailer

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose
@@ -29,6 +30,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
